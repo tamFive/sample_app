bb27584aaf48debb8f8e5a2c8d08f1f31e45079d
tamFive <tamar.schachter@fiverr.com>
2018-01-01 16:56:10 +0200

    Finish user edit, update, index, and destroy actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -26,6 +26,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", users_path
   end
 
   test "login with valid information followed by logout" do
@@ -39,6 +40,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", users_path
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
@@ -48,6 +50,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", users_path,       count: 0
   end
 
   test "login with remembering" do
