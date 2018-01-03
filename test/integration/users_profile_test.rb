d100197bdc8698a92387de558e8bca9d86f77be8
tamFive <tamar.schachter@fiverr.com>
2018-01-03 12:04:22 +0200

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -14,6 +14,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
+    assert_select 'div.stats'
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination'
     @user.microposts.paginate(page: 1).each do |micropost|
